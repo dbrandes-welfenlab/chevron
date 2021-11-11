60e5b74a9d61832530462edbea2a20f1599f0b2c
Daniel Brandes <db@flynow-aviation.com>
2021-11-11 19:48:30 +0100

    chevron/main.py:
     - revert some (wrong) changes of last commit.

--- chevron/main.py
+++ chevron/main.py
@@ -40,10 +40,9 @@ def cli_main():
         else:
             return arg
 
-    def is_dir_list(arg):
-        for d in arg:
-            if not os.path.isdir(d):
-                parser.error('The directory {0} does not exist!'.format(d))
+    def is_dir(arg):
+        if not os.path.isdir(arg):
+            parser.error('The directory {0} does not exist!'.format(arg))
         return arg
 
     parser = argparse.ArgumentParser(description=__doc__)
@@ -60,7 +59,7 @@ def cli_main():
 
     parser.add_argument('-p', '--paths', dest='partials_paths',nargs='*',
                         help='The directories where your partials reside',
-                        type=is_dir_list, default=['.'])
+                        type=is_dir, default=['.'])
 
     parser.add_argument('-e', '--ext', dest='partials_ext',
                         help='The extension for your mustache\
